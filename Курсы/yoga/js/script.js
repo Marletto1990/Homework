08f11f0d8ef28ca7cce7812380118dfa1a05f592
Evgen <you@example.com>
2019-03-19 15:54:17 +0300

    yoga lesson

---
+++ Курсы/yoga/js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded',function(){
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabcontent(a){
+        for (let i= a; i < tabContent.length; i++){
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+
+        }
+    }
+    
+    hideTabcontent(1);
+
+    function showTabcontent(b){
+        if(tabContent[b].classList.contains('hide')){
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');          
+        }
+    }
+
+    info.addEventListener('click', function(){
+        let target = event.target;
+        if(target && target.classList.contains('info-header-tab')) {
+            for ( let i = 0; i < tab.length; i ++){
+                if (target == tab[i]){
+                    hideTabcontent(0);
+                    showTabcontent(i);
+                }
+            }
+        }
+    });
+
+});
